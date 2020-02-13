429d9bde8c1021b1ecc605f4fba79007d6712bb1
666immortal <2016222042@email.szu.edu.cn>
2020-02-13 19:10:47 +0800

    完成cluster

--- cluster/cluster/cluster/main.cpp
+++ cluster/cluster/cluster/main.cpp
@@ -22,8 +22,9 @@ int main()
 	for (int i = 0; i < G.size(); i++)
 		GG.pointer[i] = G[i];
 
-	double thresh = getAdaptRate(GG, IM_HEIGHT);
-
+	double peaks_rate = getAdaptRate(GG, IM_HEIGHT);
+	double thresh = ImgProp::getThresh(peaks_rate);
+	cout << "thresh : " << thresh << endl;
 	char *peaks_mask = new char[G.size()];
 
 	for (int k = 0; k < G.size(); k++)
@@ -41,20 +42,76 @@ int main()
 	imROI1.setTo(Scalar(0));
 	imROI2.setTo(Scalar(0));
 
+	imshow("tmp", mask);
+
 	int pointsCount = countNonZero(mask);
+	cout << "Point num : " << pointsCount << endl;
 	Mat points(pointsCount, 1, CV_32FC2), labels;
 	Mat centers(pointsCount, 1, points.type());
 
-
-	for (int k = 0; k < G.size(); k++)
-		if (peaks_mask[k] != 0)
+	int tmp = 0;
+	for(int row = 0; row < mask.rows; row++)
+		for (int col = 0; col < mask.cols; col++)
 		{
-			points.at<Vec2f>(k)[0] = k / ImgProp::wid;
-			points.at<Vec2f>(k)[1] = k % ImgProp::wid;
+			if (mask.at<uchar>(row, col) != 0)
+			{
+				points.at<Vec2f>(tmp, 0)[0] = col;
+				points.at<Vec2f>(tmp, 0)[1] = row;
+				tmp++;
+			}			
 		}
 		
 	cv::kmeans(points, 3, labels, cv::TermCriteria(cv::TermCriteria::EPS | cv::TermCriteria::COUNT, 10, 1.0),
 		                             50, KMEANS_PP_CENTERS, centers);
+
+	cv::Scalar colorTab[] = {
+			cv::Scalar(0, 0, 255),
+			cv::Scalar(0, 255, 0),
+			cv::Scalar(255, 100, 100),
+			cv::Scalar(255, 0, 255),
+			cv::Scalar(0, 255, 255),
+			cv::Scalar(100, 100, 255)
+	};
+
+	Mat img(ImgProp::wid, ImgProp::len, CV_8UC3);
+	img = Scalar::all(0);
+	for (int i = 0; i < pointsCount; i++) {
+		int clusterIdx = labels.at<int>(i);
+		cv::Point ipt = points.at<cv::Point2f>(i);
+		cv::circle(img, ipt, 1, colorTab[clusterIdx], cv::FILLED, cv::LINE_AA);
+	}
+
+	int row[3] = { 0, 0, 0 }, col[3] = { 0, 0, 0 }, num[3] = { 0, 0, 0 };
+	for (int i = 0; i < pointsCount; i++)
+	{
+		int clusterIdx = labels.at<int>(i);
+		row[clusterIdx] += points.at<cv::Point2f>(i).x;
+		col[clusterIdx] += points.at<cv::Point2f>(i).y;
+		num[clusterIdx]++;
+	}
+	double mean_row[3], mean_col[3];
+	for (int i = 0; i < 3; i++)
+	{
+		mean_row[i] = row[i] / num[i];
+		mean_col[i] = col[i] / num[i];
+	}
+
+	cout << "mean_row : " << mean_row[0] << " " << mean_row[1] << " " << mean_row[2] << endl;
+	cout << "mean_col : " << mean_col[0] << " " << mean_col[1] << " " << mean_col[2] << endl;
+
+	cv::Point p[3] = { Point(mean_row[0], mean_col[0]),
+							Point(mean_row[1], mean_col[1]),
+							Point(mean_row[2], mean_col[2]), };
+
+	cv::circle(img, p[0], 3, colorTab[4], cv::FILLED, cv::LINE_AA);
+	cv::circle(img, p[1], 3, colorTab[4], cv::FILLED, cv::LINE_AA);
+	cv::circle(img, p[2], 3, colorTab[4], cv::FILLED, cv::LINE_AA);
+
+	Mat tran_img;
+	transpose(img, tran_img);
+	cv::imshow("clusters", tran_img);
+	//imwrite("cluster_res.bmp", tran_img);
+	waitKey(0);
 
 	delete[] peaks_mask;
 	return 0;
